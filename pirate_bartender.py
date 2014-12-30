e86fd68ba6602f87ada7f4203e237195077fd44c
Jared Jones <jfox1126@gmail.com>
2014-12-30 22:59:21 +0000

    more concise

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -1,67 +1,60 @@
-questions = {
-    "strong": "Do ye like yer drinks strong?",
-    "salty": "Do ye like it with a salty tang?",
-    "bitter": "Are ye a lubber who likes it bitter?",
-    "sweet": "Would ye like a bit of sweetness with yer poison?",
-    "fruity": "Are ye one for a fruity finish?"
+import random
+
+tastes = {
+  "strong": {
+    "question": "Do ye like yer drinks strong?",
+    "ingredients": ["glug of rum", "slug of whisky", "splash of gin"]
+  },
+  "salty": {
+    "question": "Do ye like it with a salty tang?",
+    "ingredients": ["olive on a stick", "salt-dusted rim", "rasher of bacon"]
+  },
+  "bitter": {
+    "question": "Are ye a lubber who likes it bitter?",
+    "ingredients": ["shake of bitters", "splash of tonic", "twist of lemon peel"]
+  },
+  "sweet": {
+    "question": "Would ye like a bit of sweetness with yer poison?",
+    "ingredients": ["sugar cube", "spoonful of honey", "spash of cola"]
+  },
+  "fruity": {
+    "question": "Are ye one for a fruity finish?",
+    "ingredients": ["slice of orange", "dash of cassis", "cherry on top"]
+  },
 }
 
-ingredients = {
-    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
-    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
-    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
-    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
-    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+names = {
+  "adjectives": ["Piercing", "Mellow", "Ragged", "Flourescent"],
+  "nouns": ["Kraken", "Squid-tentacle", "Starfish", "Sperm whale"]
 }
 
-name_adjectives = ["Piercing", "Mellow", "Ragged", "Flourescent"]
-
-name_nouns = ["Kraken", "Squid-tentacle", "Starfish", "Sperm whale"]
-
-like_list = {}  
+like_list = []  
 
 def ask_likes():
   """Prompts user to provide input and creates a list of what they like"""
   print "Ahoy! Let me ask you a few questions to find you a drink. [Respond with 'yes' or 'y']." 
-  def convert_boolean(x):
-    y = raw_input(questions[x])
-    if y.lower() == "yes" or y.lower() == "y":
-      y = True
-    else:
-      y = False
-    return y
-  global like_list
-  like_list = {
-    "strong": convert_boolean("strong"),
-    "salty": convert_boolean("salty"),
-    "bitter": convert_boolean("bitter"),
-    "sweet": convert_boolean("sweet"),
-    "fruity": convert_boolean("fruity"),
-  }
-  #print like_list
-
-import random
+  for taste, question in tastes.iteritems():
+    if raw_input(question["question"]).lower() in ["y", "yes"]:
+      global like_list
+      like_list.append(taste)
+  print ""
 
 def drink_maker():
+  """Randomly selects ingredients based on user's tastes and gives the drink a name"""
   drink_ingredients = []
-  def add_ingredients(x):
-    if like_list[x] == True:
-      random_ingredient = random.choice(ingredients[x])
-      drink_ingredients.append(random_ingredient)
-  add_ingredients("strong")
-  add_ingredients("salty")
-  add_ingredients("bitter")
-  add_ingredients("sweet")
-  add_ingredients("fruity")
-  name = random.choice(name_adjectives) + " " + random.choice(name_nouns)
+  for likes in like_list:
+    random_ingredient = random.choice(tastes[likes]["ingredients"])
+    drink_ingredients.append(random_ingredient)
+  name = random.choice(names["adjectives"]) + " " + random.choice(names["nouns"])
   print "Then try a " + name + "! Just combine:"
   for i in range(len(drink_ingredients)):
     print "A " + drink_ingredients[i]
-  #print drink_ingredients
+  print ""
   
 def have_another():
+  """Continues to ask user if s/he wants another until they say no"""
   x = raw_input("Do ye need another?")
-  while x.lower() == "yes" or x.lower() == "y":
+  while x.lower() in ["y", "yes"]:
     drink_maker()
     x = raw_input("Do ye need another?")
   else:
